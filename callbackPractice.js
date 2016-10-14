c454cda32bd75a6c23c43ac91cc3988fc56934d7
Steven Nagie <sanagie@yahoo.com>
2016-10-14 14:16:41 -0600

    Everything done, but one small bug that broke everything

--- callbackPractice.js
+++ callbackPractice.js
@@ -1,148 +1,173 @@
-/* In this repo your job is to write functions to make each function call work properly.
-Below is a sample problem
+// /* In this repo your job is to write functions to make each function call work properly.
+// Below is a sample problem
+//
+//   //code here for sayHi
+//
+//    sayHi('Hi Katie', function(thingToSay){
+//       alert(thingToSay);
+//    });
+//
+//
+// and what you should write is the sayHi function that makes the code above work,
+//
+//
+//    var sayHi = function(str, cb){
+//     cb(str);
+//    }
+//
+//    sayHi('Hi Katie', function(thingToSay){
+//       alert(thingToSay); //should alert ('Hi Katie')'
+//    });
+//
+//
+// */
+//
+//
+//
+//   //Code Here for first
+//   function first(arr, callback) {
+//     return callback(arr[0]);
+//   }
+//
+// var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+// first(names, function(firstName){
+//   console.log('The first name in names is ' + firstName);
+// });
+//
+//
+//
+// /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+//
+//
+//
+//
+//   //Code Here for last
+//
+//   function last(names, fnc) {
+//     return fnc(names[names.length - 1]);
+//   }
+//
+// last(names, function(lastName){
+//   console.log('The last name in names is ' + lastName);
+// });
+//
+//
+//
+//
+//
+// /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+//
+//
+//
+//
+//
+//
+//   //Code Here for multiply
+//
+// function multiply(a, b, fnc){
+//   var answer = a * b;
+//
+//   return fnc(answer);
+// }
+//
+//   // var multiply = function(a, b, fnc) {
+//   //   return fnc(a, b){
+//   //     return a * b;
+//   //   };
+//   // };
+//
+// multiply(4, 3, function(answer){
+//   console.log('The answer is ' + answer); //should console.log 12
+// });
+//
+//
+//
+//
+//
+// /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+//
+//
+//
+//
+//
+//   //Code Here for contains
+//
+// function contains(names, firstName, fnc) {
+//   var value;
+//   for (var i = 0; i < names.length; i++) {
+//     if (names[i] == firstName) {
+//       return fnc(true);
+//     } else {
+//       return fnc(false);
+//     }
+//   }
+// }
+//
+// contains(names, 'Colt', function(result){
+//   if(result === true){
+//     console.log('Colt is in the array');
+//   } else {
+//     console.log('Colt is not in the array');
+//   }
+// });
+//
+//
+//
+//
+//
+// /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+//
+//
+//
+//
+//     //Code Here for uniq
+//
+//     function uniq(names, fnc) {
+//       for (var i = 0; i < names.length; i++){
+//         for (var j = names.length - 1; j >= 0; j++){
+//           if (names[j] === names[i]) {
+//             delete names[j];
+//           }
+//         }
+//       }
+//       return fnc(names);
+//     }
+//
+// uniq(names, function(uniqArr){
+//   console.log('The new names array with all the duplicate items removed is ', uniqArr);
+// });
+//
+//
+//
+//
+//
+// /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+//
+//
+//
+//
+//     //Code Here for each
 
-  //code here for sayHi
-
-   sayHi('Hi Katie', function(thingToSay){
-      alert(thingToSay);
-   });
-
-
-and what you should write is the sayHi function that makes the code above work,
-
-
-   var sayHi = function(str, cb){
-    cb(str);
-   }
-
-   sayHi('Hi Katie', function(thingToSay){
-      alert(thingToSay); //should alert ('Hi Katie')'
-   });
-
-
-*/
-
-
-
-  //Code Here for first
-  function first(names, fnc) {
-    return fnc(names[1]);
-  }
-
-var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
-first(names, function(firstName){
-  console.log('The first name in names is ' + firstName);
-});
-
-
-
-/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
-
-  //Code Here for last
-
-  function last(names, fnc) {
-    return fnc(names[names.length - 1]);
-  }
-
-last(names, function(lastName){
-  console.log('The last name in names is ' + lastName);
-});
-
-
-
-
-
-/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
-
-
-
-  //Code Here for multiply
-
-  var multiply = function(a, b, fnc) {
-    return fnc(a * b);
-  }
-
-multiply(4, 3, function(answer){
-  console.log('The answer is ' + answer); //should console.log 12
-})
-
-
-
-
-
-/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
-
-
-  //Code Here for contains
-
-function contains(names, firstName, fnc) {
-  var value;
-  for (var i = 0; i < names.length; i++) {
-    if (names[i] == firstName) {
-      return true;
+    function each(names, indice, fnc){
+      return fnc(names[indice], indice);
     }
-  }
-}
-
-contains(names, 'Colt', function(result){
-  if(result === true){
-    console.log('Colt is in the array');
-  } else {
-    console.log('Colt is not in the array');
-  }
-});
-
-
-
-
-
-/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
-
-    //Code Here for uniq
-
-uniq(names, function(uniqArr){
-  console.log('The new names array with all the duplicate items removed is ', uniqArr);
-});
-
-
-
-
-
-/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
-
-    //Code Here for each
 
 each(names, function(item, indice){
-  console.log('The item in the ' + indice + ' position is ' + item)
+  console.log('The item in the ' + indice + ' position is ' + item);
 });
-
-
-
-
-
-/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-
-
-
-
-
- //code here for getUserById
-
+//
+//
+//
+//
+//
+// /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+//
+//
+//
+//
+//
+//  //code here for getUserById
+//
 var users = [
   {
     id: '12d',
@@ -163,6 +188,17 @@ var users = [
     address: '192 East 32 North'
   },
 ];
+
+function getUserById(users, val, fnc){
+
+  for (var i = 0; i < users.length; i++){
+    for (var prop in users[i]){
+      if (users[i][prop] === val){
+        return fnc(users[i]);
+      }
+    }
+  }
+}
 
 getUserById(users, '16t', function(user){
   console.log('The user with the id 16t has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address);
