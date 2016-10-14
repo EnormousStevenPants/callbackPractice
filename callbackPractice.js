ffd4b15f0ca223f5b9c0c5dc313e98b92c324b56
Steven Nagie <sanagie@yahoo.com>
2016-10-14 15:26:00 -0600

    Finished everything up about 15:30 on the afternoon of the 14th. Everything tests good.

--- callbackPractice.js
+++ callbackPractice.js
@@ -1,115 +1,115 @@
-// /* In this repo your job is to write functions to make each function call work properly.
-// Below is a sample problem
-//
-//   //code here for sayHi
-//
-//    sayHi('Hi Katie', function(thingToSay){
-//       alert(thingToSay);
-//    });
-//
-//
-// and what you should write is the sayHi function that makes the code above work,
-//
-//
-//    var sayHi = function(str, cb){
-//     cb(str);
-//    }
-//
-//    sayHi('Hi Katie', function(thingToSay){
-//       alert(thingToSay); //should alert ('Hi Katie')'
-//    });
-//
-//
-// */
-//
-//
-//
-//   //Code Here for first
-//   function first(arr, callback) {
-//     return callback(arr[0]);
-//   }
-//
-// var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
-// first(names, function(firstName){
-//   console.log('The first name in names is ' + firstName);
-// });
-//
-//
-//
-// /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-//
-//
-//
-//
-//   //Code Here for last
-//
-//   function last(names, fnc) {
-//     return fnc(names[names.length - 1]);
-//   }
-//
-// last(names, function(lastName){
-//   console.log('The last name in names is ' + lastName);
-// });
-//
-//
-//
-//
-//
-// /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-//
-//
-//
-//
-//
-//
-//   //Code Here for multiply
-//
-// function multiply(a, b, fnc){
-//   var answer = a * b;
-//
-//   return fnc(answer);
-// }
-//
-//   // var multiply = function(a, b, fnc) {
-//   //   return fnc(a, b){
-//   //     return a * b;
-//   //   };
-//   // };
-//
-// multiply(4, 3, function(answer){
-//   console.log('The answer is ' + answer); //should console.log 12
-// });
-//
-//
-//
-//
-//
-// /* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
-//
-//
-//
-//
-//
-//   //Code Here for contains
-//
-// function contains(names, firstName, fnc) {
-//   var value;
-//   for (var i = 0; i < names.length; i++) {
-//     if (names[i] == firstName) {
-//       return fnc(true);
-//     } else {
-//       return fnc(false);
-//     }
-//   }
-// }
-//
-// contains(names, 'Colt', function(result){
-//   if(result === true){
-//     console.log('Colt is in the array');
-//   } else {
-//     console.log('Colt is not in the array');
-//   }
-// });
+/* In this repo your job is to write functions to make each function call work properly.
+Below is a sample problem
+
+  //code here for sayHi
+
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay);
+   });
+
+
+and what you should write is the sayHi function that makes the code above work,
+
+
+   var sayHi = function(str, cb){
+    cb(str);
+   }
+
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay); //should alert ('Hi Katie')'
+   });
+
+
+*/
+
+
+
+  //Code Here for first
+  function first(arr, callback) {
+    return callback(arr[0]);
+  }
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+first(names, function(firstName){
+  console.log('The first name in names is ' + firstName);
+});
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+  //Code Here for last
+
+  function last(names, fnc) {
+    return fnc(names[names.length - 1]);
+  }
+
+last(names, function(lastName){
+  console.log('The last name in names is ' + lastName);
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+
+  //Code Here for multiply
+
+function multiply(a, b, fnc){
+  var answer = a * b;
+
+  return fnc(answer);
+}
+
+  // var multiply = function(a, b, fnc) {
+  //   return fnc(a, b){
+  //     return a * b;
+  //   };
+  // };
+
+multiply(4, 3, function(answer){
+  console.log('The answer is ' + answer); //should console.log 12
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+  //Code Here for contains
+
+function contains(names, firstName, fnc) {
+  var value;
+  for (var i = 0; i < names.length; i++) {
+    if (names[i] == firstName) {
+      return fnc(true);
+    } else {
+      return fnc(false);
+    }
+  }
+}
+
+contains(names, 'Colt', function(result){
+  if(result === true){
+    console.log('Colt is in the array');
+  } else {
+    console.log('Colt is not in the array');
+  }
+});
 //
 //
 //
@@ -122,20 +122,20 @@
 //
 //     //Code Here for uniq
 //
-//     function uniq(names, fnc) {
-//       for (var i = 0; i < names.length; i++){
-//         for (var j = names.length - 1; j >= 0; j++){
-//           if (names[j] === names[i]) {
-//             delete names[j];
-//           }
-//         }
-//       }
-//       return fnc(names);
-//     }
-//
-// uniq(names, function(uniqArr){
-//   console.log('The new names array with all the duplicate items removed is ', uniqArr);
-// });
+    function uniq(names, fnc) {
+      for (var i = 0; i < names.length; i++){
+        for (var j = names.length - 1; j > i; j--){
+          if (names[j] === names[i]) {
+            names.splice(j, 1);
+          }
+        }
+      }
+      return fnc(names);
+    }
+
+uniq(names, function(uniqArr){
+  console.log('The new names array with all the duplicate items removed is ', uniqArr);
+});
 //
 //
 //
@@ -148,9 +148,12 @@
 //
 //     //Code Here for each
 
-    function each(names, indice, fnc){
-      return fnc(names[indice], indice);
-    }
+function each(names, fnc){
+
+  for (var i = 0; i < names.length; i++){
+    fnc(names[i], i);
+  }
+}
 
 each(names, function(item, indice){
   console.log('The item in the ' + indice + ' position is ' + item);
